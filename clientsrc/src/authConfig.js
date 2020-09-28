cce929c73f8fd9cf75bf770f5e7ab7e078cf83ac
Nehemiah Boone <nehemiah.boone@gmail.com>
2020-09-28 14:03:42 -0600

    auth

--- clientsrc/src/authConfig.js
+++ clientsrc/src/authConfig.js
@@ -1,3 +1,3 @@
 export const domain = "nehemiahboone.us.auth0.com";
-export const audience = "https://NehemiahAPI/";
+export const audience = "nehemiahs-api";
 export const clientId = "0Rne5SzwB0Pq0pWNQmZpm5kPjGrx5upg";
